653cb4a37972aa5f31558ec1210d2be2233423ef
AA-wahab1 <ahmedabdulwahab092@gmail.com>
2021-08-06 10:13:48 +0500

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  final int days = 30;
+  final String name = "flutter app";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("catalog App"),),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of my $name"),
+        ),
+      ),
+    drawer: Drawer()),;
+  }
+}
