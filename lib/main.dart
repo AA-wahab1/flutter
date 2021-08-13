4e0e7e81d1795d918bab3c72b0934d937145855a
AA-wahab1 <ahmedabdulwahab092@gmail.com>
2021-08-13 07:14:06 +0500

    day 6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 
 void main() {
   runApp(Myapp());
@@ -12,19 +12,33 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-       themeMode: ThemeMode.light,
-      theme:ThemeData(primarySwatch: Colors.deepPurple,
-      primaryTextTheme: GoogleFonts.latoTextTheme()
-      ),
       
-      initialRoute: "/home",
-     routes: { 
-     "/":(context) => Loginpage(),
-     "/home":(context) => Homepage(),
-     "/Login":(context)=> Loginpage()
-     },
-    )
-    ;
+      
+      debugShowCheckedModeBanner: false,
+      
+      
+      
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+          primaryTextTheme: GoogleFonts.latoTextTheme()),
+          
+
+
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => Loginpage(),
+        MyRoutes.homeRoute: (context) => Homepage(),
+        MyRoutes.loginRoute: (context) => Loginpage()
+      },
+    
+
+          
+          
+      
+          
+      
+    
+    );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
-class Loginpage extends StatelessWidget {
+class Loginpage extends StatefulWidget {
+  @override
+  _LoginpageState createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
-        color: Colors.white,
+     
+
+     
+        color: Colors.blue,
+       
         child: Column(
           children: [
             Image.asset(
@@ -15,36 +29,77 @@ class Loginpage extends StatelessWidget {
               height: 20.0,
             ),
             Text(
-              "HI-PURE",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              "HI-PURE $name",
+              style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.blue[900]),
             ),
             Padding(
               padding:
                   const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
               child: Column(
-                children: [
+                children: [ 
                   TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Enter Username", labelText: "Username"),
-                  ),
+                      decoration: InputDecoration(
+                          hintText: "Enter Username", labelText: "Username"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      }),
+
                   TextFormField(
                     obscureText: true,
                     decoration: InputDecoration(
                         hintText: "Enter Password", labelText: "Password"),
                   ),
                   SizedBox(
-                    height: 20.0,
+                    height: 40.0,
                   ),
-                  ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom
-                    (backgroundColor: Colors.blue),
-                    onPressed: () {},
-                  )
+
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: Container(
+                      width: 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(30)),
+                    ),
+                  ),
+
+
+                  // ElevatedButton(
+                  //   child: Text("Login",
+                  //       style: TextStyle(
+                  //           color: Colors.blue[900],
+                  //           fontWeight: FontWeight.w700)),
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize: Size(100, 40),
+                  //     backgroundColor: Colors.lightBlue[200],
+                  //   ),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  // )
                 ],
               ),
             ),
           ],
-        ));
+        ),
+       );
   }
 }
